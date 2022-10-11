ddb1506cab1bbf38c8a7016a13512688c26aa203
Lucas Barrios <lucasarielbarrios019@gmail.com>
2022-10-10 23:06:00 -0300

    Segunda Entrega

--- script.js
+++ script.js
@@ -1,21 +1,27 @@
-let LibroA = "Libro A"
-let precioLibroA = "100"
-let stockLibroA = 5
+function Producto(nombre, precio, stock) {
+    this.nombre = nombre;
+    this.precio = precio;
+    this.stock = stock;
+    this.actualizarStock = function(cantidad){
+        this.stock -= cantidad
+    }
+}
 
-let LibroB = "Libro B"
-let precioLibroB = "150"
-let stockLibroB = 6
+let libroA = new Producto("Libro A", 100, 10)
+let libroB = new Producto("Libro B", 150, 6)  
+let libroC = new Producto("Libro C", 75, 8)
+let libroD = new Producto("Libro D", 90, 0)
 
-let LibroC = "Libro C"
-let precioLibroC = "75"
-let stockLibroC = 8
+let listaLibros = [libroA, libroB,  libroC, libroD]
 
+let listaStock = listaLibros.filter((libro) => libro.stock > 0)
 
+let listaNombres = listaStock.map ((libro) => libro.nombre)
 
 let precioTotal = 0
 
 
-alert("Estos son nuestros productos: \n - Libro A\n - Libro B\n - Libro C")
+alert("Estos son nuestros productos: \n - " + listaNombres.join ("\n -"))
 
 
 function calculoPrecio(cantidad, precio){
@@ -40,18 +46,20 @@ for(let i = 0; i < cantidadCompra; i = i + 1){
     let libroCompra = prompt("Ingrese que producto quiere comprar: \n - Libro A\n - Libro B\n - Libro C")
     
     if(libroCompra == 'Libro A'){
-        let cantidadLibroA= prompt("ingrese que cantidad de " + LibroA + " desea comprar:")
-        calculoStock(cantidadLibroA, stockLibroA, precioLibroA)
+        let cantidadLibroA= prompt("ingrese que cantidad de " + libroA.nombre + " desea comprar:")
+        calculoStock(cantidadLibroA, libroA.stock, libroA.precio)
+        libroA.actualizarStock(cantidadLibroA)
         
     }
     else if(libroCompra == 'Libro B'){
-        let cantidadLibroB = prompt("ingrese que cantidad de " + LibroB + " desea comprar:")
-        calculoStock(cantidadLibroB, stockLibroB, precioLibroB)
-        
+        let cantidadLibroB = prompt("ingrese que cantidad de " + libroB.nombre + " desea comprar:")
+        calculoStock(cantidadLibroB, libroB.stock, libroB.precio)
+        libroA.actualizarStock(cantidadLibroB)    
     }
     else if(libroCompra == 'Libro C'){
-        let cantidadLibroC = prompt("ingrese que cantidad de " + LibroC + " desea comprar:")
-        calculoStock(cantidadLibroC, stockLibroC, precioLibroC)
+        let cantidadLibroC = prompt("ingrese que cantidad de " + libroC.nombre + " desea comprar:")
+        calculoStock(cantidadLibroC, libroC.stock, libroC.precio)
+        libroA.actualizarStock(cantidadLibroC)
     }
     else{
         alert("No contamos con ese Libro a la venta")
